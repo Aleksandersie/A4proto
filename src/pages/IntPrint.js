487d890d33deafed22d53726f448aaba3640535f
Aleksandersie <aleksander.sie@gmail.com>
2022-09-18 22:53:12 +0300

    Type card

--- src/pages/IntPrint.js
+++ src/pages/IntPrint.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import SideBar from "../components/SideBar/SideBar";
+import TopStepper from "../components/TopStepper/TopStepper";
+import TopBreadCrumbs from "../components/TopBreadCrumbs/TopBreadCrumbs";
+import CategoryCard from "../components/CategoryCard/CategoryCard";
 
 const IntPrint = () => {
-  return <div>test</div>;
+  return (
+    <Container>
+      <Row>
+        <Col className="col-3 mt-4">
+          <SideBar />
+        </Col>
+        <Col className="col-9 mt-4">
+          <TopStepper />
+          <TopBreadCrumbs />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default IntPrint;
