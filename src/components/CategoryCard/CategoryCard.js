81107f3f27e38316c2c98cb663dd9c7fa37075c0
Aleksandersie <aleksander.sie@gmail.com>
2022-09-18 11:53:10 +0300

    Type card

--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -6,16 +6,31 @@ import { INT_PRINT } from "../../routeConst/routeConst";
 
 const CategoryCard = () => {
   const category = [
-    { name: "Интерьерная печать", path: INT_PRINT },
-    { name: "Цифровая печать", path: INT_PRINT },
-    { name: "Плоттерная резка", path: INT_PRINT },
+    {
+      name: "Интерьерная печать",
+      path: INT_PRINT,
+      imgLink:
+        "https://a4-yug.ru/wp-content/uploads/2020/10/maxresdefault-e1603785569284.jpg",
+    },
+    {
+      name: "Цифровая печать",
+      path: INT_PRINT,
+      imgLink: "https://a4-yug.ru/wp-content/uploads/2020/10/flaery.jpg",
+    },
+    {
+      name: "Плоттерная резка",
+      path: INT_PRINT,
+      imgLink:
+        "https://a4-yug.ru/wp-content/uploads/2020/10/plotternaya_rezka4.jpg",
+    },
   ];
+
   return (
     <Paper className="mt-4" elevation={3} className="ps-5 pe-5 mt-4 pb-5 pt-4">
       <Row className="d-flex justify-content-around">
         {category.map((cat) => (
           <Col className="col-4 mt-3" key={cat.name}>
-            <TypeCard name={cat.name} path={cat.path} />
+            <TypeCard name={cat.name} path={cat.path} imgLink={cat.imgLink} />
           </Col>
         ))}
       </Row>
--- src/components/CategoryCard/PrintCutCard/TypeCard.js
+++ src/components/CategoryCard/PrintCutCard/TypeCard.js
@@ -1,22 +1,35 @@
 import img from "../../../Static/etiketki1.jpg";
 import { Button } from "react-bootstrap";
 import { Card, CardContent, CardMedia } from "@mui/material";
-import { Link, NavLink } from "react-router-dom";
-import { MAIN_ROUTE, INT_PRINT } from "../../../routeConst/routeConst";
+import { NavLink } from "react-router-dom";
 
-const TypeCard = ({ name, path }) => {
+import { useState } from "react";
+
+const TypeCard = ({ name, path, imgLink }) => {
   const test2 = () => {
     setTimeout(() => {
       console.log("Done");
     }, 1000);
   };
+  const [text, setText] = useState("");
+  const add = () => {
+    setText("qwerty");
+  };
+  const out = () => {
+    setText("");
+  };
 
   return (
-    <Card sx={{ maxWidth: 345 }} style={{ opacity: 0.2 }}>
-      <CardMedia component="img" height="140" image={img} alt="green iguana" />
+    <Card sx={{ maxWidth: 345 }} onMouseEnter={add} onMouseLeave={out}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={imgLink}
+        alt="green iguana"
+      />
       <CardContent style={{ textAlign: "center" }}>
         <h5>{name}</h5>
-        <p></p>
+        <p>{text}</p>
         <NavLink to={path}>
           <Button variant="warning">Выбрать</Button>
         </NavLink>
