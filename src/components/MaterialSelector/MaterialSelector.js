639d30251569b139e3da0a139583f013449e27dd
AleksanderSav <113454243+AleksanderSav@users.noreply.github.com>
2022-09-22 15:21:16 +0300

    material button

--- src/components/MaterialSelector/MaterialSelector.js
+++ src/components/MaterialSelector/MaterialSelector.js
@@ -3,8 +3,9 @@ import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import s from "../CalcBlock/CalckBlock.module.css";
 import TypeCard from "../CategoryCard/TypeCard/TypeCard";
 import { Context } from "../../index";
+import { observer } from "mobx-react-lite";
 
-const MaterialSelector = () => {
+const MaterialSelector = observer(() => {
   const { materialList } = useContext(Context);
   console.log({ materialList });
   return (
@@ -18,22 +19,24 @@ const MaterialSelector = () => {
           </Card.Header>
           <Card.Body>
             <Card.Title></Card.Title>
-            <Row className="d-flex justify-content-around">
+            <div>
               {materialList.list.map((l) => (
                 <Button
                   key={l.id}
-                  className="mt-3"
+                  variant="warning"
+                  className="ms-3"
+                  disabled={l.id === materialList.selectedMaterial.id}
                   onClick={() => materialList.setSelectedMaterial(l)}
                 >
                   {l.name}
                 </Button>
               ))}
-            </Row>
+            </div>
           </Card.Body>
         </Card>
       </Container>
     </div>
   );
-};
+});
 
 export default MaterialSelector;
