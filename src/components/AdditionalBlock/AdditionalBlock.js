4d4d2d881b60662569bee5450ad26194f13b458d
Aleksandersie <aleksander.sie@gmail.com>
2022-10-04 23:26:14 +0300

    add block

--- src/components/AdditionalBlock/AdditionalBlock.js
+++ src/components/AdditionalBlock/AdditionalBlock.js
@@ -1,47 +1,47 @@
-import { useContext } from "react";
+import {useContext, useState} from "react";
 import { Card, ToggleButton, Image } from "react-bootstrap";
 import { Context } from "../..";
 
 const AdditionalBlock = () => {
+
+  const [lamination,setLamination] = useState(false)
+  function lam(e){
+     setLamination(e)
+     console.log(lamination)
+  }
   const { AddService } = useContext(Context);
   return (
     <Card className="mt-5">
       <Card.Header style={{ textAlign: "center" }}>
         <h4>Дополнительные услуги</h4>
       </Card.Header>
-      <div className="d-flex">
+      <div className="d-flex justify-content-center gap-5">
         {AddService.addList.map((add) => (
           <Card
-            className="p-3 mt-3"
+            className="mt-3 mb-3 pb-1"
             style={{ width: 280, textAlign: "center" }}
           >
-            <div
-              style={{
-                overflow: "hidden",
-                width: 250,
-                height: 130,
-                borderRadius: 10
-              }}
-              className="shadow"
-            >
-              <Image src={add.img} width={300}></Image>
-            </div>
-            {add.desc}
+              <Card.Header style={{fontWeight:500}} >{add.name}</Card.Header>
+              <Card.Img variant="top" src={add.img} style={{borderRadius:0,height:150}}></Card.Img>
+              <Card.Text style={{marginTop:10}}>{add.desc}</Card.Text>
+
+              <ToggleButton
+        className="mb-3 ms-3 me-3"
+        id="toggle-check"
+        type="checkbox"
+        variant="outline-warning"
+        checked={lamination}
+        value="1"
+         onChange={(e) => setLamination(e.currentTarget.checked)}
+      >
+        Checked
+      </ToggleButton>
+
           </Card>
         ))}
       </div>
 
-      {/* <ToggleButton
-        className="mb-2"
-        id="toggle-check"
-        type="checkbox"
-        variant="outline-warning"
-        // checked={checked}
-        value="1"
-        // onChange={(e) => setChecked(e.currentTarget.checked)}
-      >
-        Checked
-      </ToggleButton> */}
+
     </Card>
   );
 };
