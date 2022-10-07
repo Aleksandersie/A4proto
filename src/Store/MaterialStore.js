e1bffc313563fdee5194de08a9934ed023a16533
AleksanderSav <113454243+AleksanderSav@users.noreply.github.com>
2022-10-07 15:04:18 +0300

    material list

--- src/Store/MaterialStore.js
+++ src/Store/MaterialStore.js
@@ -9,26 +9,26 @@ export default class MaterialStore {
         id: 1,
         name: "Плёнка",
         path: INT_PRINT_CALC,
-        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. ",
+        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. "
       },
       {
         id: 2,
         name: "Баннер",
         path: INT_PRINT_CALC,
-        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. ",
+        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. "
       },
       {
         id: 3,
         name: "Бумага",
         path: INT_PRINT_CALC,
-        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. ",
+        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. "
       },
       {
         id: 4,
         name: "Беклит",
         path: INT_PRINT_CALC,
-        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. ",
-      },
+        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. "
+      }
     ];
     this._vinyl = [
       {
@@ -37,7 +37,7 @@ export default class MaterialStore {
         id: 1,
         description: "Имеет глянцевую поверхность и блеск",
         imgLink:
-          "https://a.allegroimg.com/s720/11e1c1/5bbee43445ef9e097d766a0903c1/ORACAL-Folia-Klejowa-Meblowa-BIALA-POLYSK-1mx50cm-Marka-DecoMeister",
+          "https://a.allegroimg.com/s720/11e1c1/5bbee43445ef9e097d766a0903c1/ORACAL-Folia-Klejowa-Meblowa-BIALA-POLYSK-1mx50cm-Marka-DecoMeister"
       },
       {
         name: "Белая матовая пленка",
@@ -45,7 +45,7 @@ export default class MaterialStore {
         id: 2,
         description: "Имеет матовую поверхность и блеск отсутствует",
         imgLink:
-          "https://www.remex.ru/images/stories/virtuemart/product/50_v_orajet-3640.jpg",
+          "https://www.remex.ru/images/stories/virtuemart/product/50_v_orajet-3640.jpg"
       },
       {
         name: "Прозрачная глянцевая пленка",
@@ -53,7 +53,7 @@ export default class MaterialStore {
         id: 3,
         description: "Прозрачная пленка, имеет блеск",
         imgLink:
-          "https://www.virage24.ru/upload/iblock/cfc/cfcd2106106c5a2a3562df68b6c3b7d0.jpeg",
+          "https://www.virage24.ru/upload/iblock/cfc/cfcd2106106c5a2a3562df68b6c3b7d0.jpeg"
       },
       {
         name: "Прозрачная матовая пленка",
@@ -61,8 +61,8 @@ export default class MaterialStore {
         id: 4,
         description: "Прозрачная пленка, блеск отсутствует",
         imgLink:
-          "https://fokus-groupp.ru/wp-content/uploads/2022/05/plenka-prozrachnaja-300x225.jpg",
-      },
+          "https://fokus-groupp.ru/wp-content/uploads/2022/05/plenka-prozrachnaja-300x225.jpg"
+      }
     ];
     this._specVinyl = [
       {
@@ -71,26 +71,59 @@ export default class MaterialStore {
         id: 1,
         description: "Имеет глянцевую поверхность и блеск",
         imgLink:
-          "https://redline32.ru/assets/cache_image/images/Products/print_materials/perforated-film_1920x0_6c4.jpg",
+          "https://redline32.ru/assets/cache_image/images/Products/print_materials/perforated-film_1920x0_6c4.jpg"
       },
       {
         name: "С черным клевым слоем",
         brand: "OraJet 3640M",
         id: 2,
-        description: "Имеет матовую поверхность и блеск отсутствует",
+        description: "Имеет матовую поверхность и блеск отсутствует"
       },
       {
         name: "Полимерная пленка",
         brand: "OraJet 3640 Clear G",
         id: 3,
-        description: "Прозрачная пленка, имеет блеск",
+        description: "Прозрачная пленка, имеет блеск"
       },
       {
         name: "Цветная пленка",
         brand: "OraJet 3640 Clear M",
         id: 4,
-        description: "Прозрачная пленка, блеск отсутствует",
+        description: "Прозрачная пленка, блеск отсутствует"
+      }
+    ];
+    this._intPrintMaterial = [
+      {
+        id: 1,
+        name: "Плёнка",
+        path: INT_PRINT_CALC,
+        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. ",
+        img:
+          "https://a4-yug.ru/wp-content/uploads/2020/10/IMG_7250-1-scaled.jpg"
       },
+      {
+        id: 2,
+        name: "Баннер",
+        path: INT_PRINT_CALC,
+        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. ",
+        img: "https://a4-yug.ru/wp-content/uploads/2020/10/banner2.jpeg"
+      },
+      {
+        id: 3,
+        name: "Бумага",
+        path: INT_PRINT_CALC,
+        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. ",
+        img:
+          "https://a4-yug.ru/wp-content/uploads/2020/10/foto_format1-150x150.jpg"
+      },
+      {
+        id: 4,
+        name: "Беклит",
+        path: INT_PRINT_CALC,
+        desc: "Самоклеящуюся пленку используют для рекламы, декора, хобби. ",
+        img:
+          "https://a4-yug.ru/wp-content/uploads/2020/10/backlit3-e1603784572542.jpg"
+      }
     ];
     this._selectedMaterial = {};
     this._selectedVinyl = {};
@@ -128,5 +161,11 @@ export default class MaterialStore {
   }
   get selectedSpecVinyl() {
     return this._selectedSpecVinyl;
+  }
+  setIntPrintMaterial(material) {
+    this._intPrintMaterial = material;
+  }
+  get intPrintMaterial() {
+    return this._intPrintMaterial;
   }
 }
--- src/components/MaterialTypeList/MaterialTypeList.js
+++ src/components/MaterialTypeList/MaterialTypeList.js
@@ -1,14 +1,20 @@
 import { Card } from "react-bootstrap";
+import { Context } from "../..";
+import React, { useContext } from "react";
+import { observer } from "mobx-react-lite";
 
-const MaterialTypeList = () => {
+const MaterialTypeList = observer(() => {
+  const { materialList } = useContext(Context);
   return (
     <Card style={{ textAlign: "center" }}>
       <Card.Header>
         <h4>Выберите тип материала</h4>
       </Card.Header>
-      <Card>123</Card>
+      {materialList.intPrintMaterial.map((mat) => (
+        <Card>{mat.name}</Card>
+      ))}
     </Card>
   );
-};
+});
 
 export default MaterialTypeList;
